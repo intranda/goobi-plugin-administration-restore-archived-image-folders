fbe0ceb6e2ce04adcd6be7dde76e330c6b96ad2c
Robert Sehr <robert.sehr@intranda.com>
2024-06-18 09:38:54 +0200

    removed unused files, restore archived folder

--- module-base/src/main/java/de/intranda/goobi/plugins/RestorearchivedimagefoldersAdministrationPlugin.java
+++ module-base/src/main/java/de/intranda/goobi/plugins/RestorearchivedimagefoldersAdministrationPlugin.java
@@ -21,19 +21,18 @@ import org.omnifaces.cdi.PushContext;
 
 import de.sub.goobi.config.ConfigurationHelper;
 import de.sub.goobi.persistence.managers.ProcessManager;
+import io.goobi.workflow.api.connection.SftpUtils;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.log4j.Log4j2;
-import net.schmizz.sshj.SSHClient;
-import net.schmizz.sshj.sftp.RemoteResourceInfo;
-import net.schmizz.sshj.sftp.SFTPClient;
-import net.schmizz.sshj.transport.verification.PromiscuousVerifier;
-import net.schmizz.sshj.userauth.keyprovider.KeyProvider;
 import net.xeoh.plugins.base.annotations.PluginImplementation;
 
 @PluginImplementation
 @Log4j2
 public class RestorearchivedimagefoldersAdministrationPlugin implements IAdministrationPlugin, IPushPlugin {
+
+    private static final long serialVersionUID = -4556219498290440568L;
+
     private PushContext pusher;
 
     @Getter
@@ -57,10 +56,10 @@ public class RestorearchivedimagefoldersAdministrationPlugin implements IAdminis
     private int percentDone;
 
     @Getter
-    private List<RestoreFolderInformation> restoreInfos = new ArrayList<>();
+    private transient List<RestoreFolderInformation> restoreInfos = new ArrayList<>();
 
     @Getter
-    private RestoreFolderInformation currentlyRestoring;
+    private transient RestoreFolderInformation currentlyRestoring;
 
     @Override
     public PluginType getType() {
@@ -72,35 +71,23 @@ public class RestorearchivedimagefoldersAdministrationPlugin implements IAdminis
         return "/uii/plugin_administration_restorearchivedimagefolders.xhtml";
     }
 
-    /**
-     * Constructor
-     */
-    public RestorearchivedimagefoldersAdministrationPlugin() {
-        log.info("Sample admnistration plugin started");
-    }
-
     public void execute() throws ConfigurationException {
-        //        log.info("Starting to excute.");
         String query = FilterHelper.criteriaBuilder(filter, false, null, null, null, true, false);
         List<Integer> tempProcesses = ProcessManager.getIdsForFilter(query);
 
         restoreInfos = tempProcesses.stream()
                 .map(id -> new RestoreFolderInformation(id))
                 .collect(Collectors.toList());
-        //        log.info("The length of restoreInfos is :" + restoreInfos.size());
         for (RestoreFolderInformation restoreInfo : restoreInfos) {
             List<Path> archiveInformationFiles = getArchiveInformationFilesForProcess(restoreInfo.getProcessId());
-            //            log.info("The length of archiveInformationFiles is :" + archiveInformationFiles.size());
             int numberOfImages = 0;
             for (Path archiveInformationFile : archiveInformationFiles) {
                 XMLConfiguration xmlConf = new XMLConfiguration(archiveInformationFile.toFile());
                 numberOfImages += xmlConf.getInt("numberOfImages", 0);
-                //                log.info("numberOfImages is :" + numberOfImages);
             }
             totalImagesToRestore += numberOfImages;
             restoreInfo.setImagesToRestore(numberOfImages);
         }
-        //        log.info("The number of images to restore is :" + totalImagesToRestore);
         Runnable runnable = () -> {
             for (RestoreFolderInformation restoreInfo : restoreInfos) {
                 List<Path> archiveInformationFiles = getArchiveInformationFilesForProcess(restoreInfo.getProcessId());
@@ -109,13 +96,13 @@ public class RestorearchivedimagefoldersAdministrationPlugin implements IAdminis
                     try {
                         restoreFolder(restoreInfo, archiveInformationFile);
                     } catch (ConfigurationException | IOException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
+                        log.error(e);
                         continue;
                     }
                     try {
                         Files.delete(archiveInformationFile);
                     } catch (IOException e) {
+                        log.error(e);
                     }
                 }
             }
@@ -127,15 +114,23 @@ public class RestorearchivedimagefoldersAdministrationPlugin implements IAdminis
     private void restoreFolder(RestoreFolderInformation info, Path archiveInformationFile) throws ConfigurationException, IOException {
         Instant lastPush = Instant.now();
         XMLConfiguration xmlConf = new XMLConfiguration(archiveInformationFile.toFile());
-        try (SSHClient sshClient = createSSHClient(xmlConf); SFTPClient sftpClient = sshClient.newSFTPClient()) {
+        try (SftpUtils sftpClient = new SftpUtils(xmlConf.getString("user"), xmlConf.getString("privateKeyLocation"),
+                xmlConf.getString("privateKeyPassphrase"), xmlConf.getString("host"), xmlConf.getInt("port"), xmlConf.getString("knownHostsFile"))) {
+
             Path remotePath =
                     Paths.get(Integer.toString(info.getProcessId()), "images", archiveInformationFile.getFileName().toString().replace(".xml", ""));
             Path localPath = Paths.get(ConfigurationHelper.getInstance().getGoobiFolder(), "metadata").resolve(remotePath);
             Files.createDirectories(localPath);
-            List<RemoteResourceInfo> remoteFiles = sftpClient.ls(remotePath.toString());
-            info.setImagesToRestore(remoteFiles.size());
-            for (RemoteResourceInfo remoteFile : remoteFiles) {
-                sftpClient.get(remoteFile.getPath(), localPath.resolve(remoteFile.getName()).toString());
+
+            sftpClient.changeRemoteFolder(remotePath.toString());
+            List<String> remoteFiles = sftpClient.listContent();
+
+            info.setImagesToRestore(remoteFiles.size() - 2);
+            for (String remoteFile : remoteFiles) {
+                if (".".equals(remoteFile) || "..".equals(remoteFile)) {
+                    continue;
+                }
+                sftpClient.downloadFile(remoteFile, localPath);
                 info.setImagesRestored(info.getImagesRestored() + 1);
                 totalImagesRestored++;
                 percentDone = (int) (((double) totalImagesRestored / (double) totalImagesToRestore) * 100);
@@ -144,30 +139,19 @@ public class RestorearchivedimagefoldersAdministrationPlugin implements IAdminis
                     pusher.send("update");
                 }
             }
-            for (RemoteResourceInfo remoteFile : remoteFiles) {
-                sftpClient.rm(remoteFile.getPath());
+            for (String remoteFile : remoteFiles) {
+                if (".".equals(remoteFile) || "..".equals(remoteFile)) {
+                    continue;
+                }
+                sftpClient.deleteFile(remoteFile);
             }
-            sftpClient.rmdir(remotePath.toString());
-            sftpClient.rmdir(remotePath.getParent().toString());
-            sftpClient.rmdir(remotePath.getParent().getParent().toString());
+            sftpClient.changeRemoteFolder("..");
+            sftpClient.deleteFolder(remotePath.getFileName().toString());
+            sftpClient.changeRemoteFolder("..");
+            sftpClient.deleteFolder(remotePath.getParent().getFileName().toString());
+            sftpClient.changeRemoteFolder("..");
+            sftpClient.deleteFolder(remotePath.getParent().getParent().getFileName().toString());
         }
-
-    }
-
-    private SSHClient createSSHClient(XMLConfiguration xmlConf) throws IOException {
-        SSHClient client = new SSHClient();
-        client.addHostKeyVerifier(new PromiscuousVerifier());
-        try {
-            log.info("Start to connect");
-            client.connect(xmlConf.getString("host"));
-            //            log.info("host is :" + xmlConf.getString("host"));
-            //            client.authPublickey(xmlConf.getString("user"));
-            KeyProvider kp = client.loadKeys(xmlConf.getString("privateKeyLocation"), xmlConf.getString("privateKeyPassphrase"));
-            client.authPublickey(xmlConf.getString("user"), kp);
-        } catch (net.schmizz.sshj.userauth.UserAuthException e) {
-            log.error(e);
-        }
-        return client;
     }
 
     private List<Path> getArchiveInformationFilesForProcess(Integer processId) {
